1a9f49cb649f22606fa768a0fb156c5020c56fff
ander <ander_the_wood@mail.ru>
2022-09-19 22:22:01 +0200

    v0.1.0

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from distutils.core import setup
 
+
+with open("README.md", "r", encoding="utf-8") as file:
+    readme = file.read()
+
+
 setup(
     name="request2postman",
     packages=["request2postman"],
     version="0.1.0",
     license="MIT",
     description="Lib that generates postman collections out of requests",
+    long_description=readme,
+    long_description_content_type='text/markdown',
     author="Egor Gondurov",
     author_email="ander_the_wood@mail.ru",
     url="https://github.com/Ander813",
-    download_url="",
+    download_url="https://github.com/Ander813/request2postman/archive/refs/tags/0.1.0.tar.gz",
     keywords=["requests", "postman", "generate postman collections"],
     install_requires=[
         "requests",
