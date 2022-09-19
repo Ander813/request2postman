48a35e5d1e54df1cac40e78cd95bc89f8eb4f29d
ander <ander_the_wood@mail.ru>
2022-09-19 23:03:31 +0200

    v0.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as file:
 setup(
     name="request2postman",
     packages=["request2postman"],
-    version="0.1.0",
+    version="0.1.1",
     license="MIT",
     description="Lib that generates postman collections out of requests",
     long_description=readme,
